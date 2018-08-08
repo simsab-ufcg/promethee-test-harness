6051937d63a67d4037203cfe1177270b9f46788e
Filipe Mendonça <filipetrm@gmail.com>
2018-08-08 10:48:36 -0300

    build with makefile and ignoring binaries

--- execute_promethee.sh
+++ execute_promethee.sh
@@ -3,8 +3,11 @@ INPUT_PATH=$2
 OUTPUT_PATH=$3
 EXPECTED_OUTPUT_PATH=$4
 
+# calls the comparator compilation script
+sh build.sh
+
 # executes the promethee with given inputs and output paths
 /$PROGRAM_PATH < $INPUT_PATH > $OUTPUT_PATH
 
 # executes the comparator with given the expected output path
-./diff/comparator < $EXPECTED_OUTPUT_PATH < $OUTPUT_PATH
+./diff/bin/comparator < $EXPECTED_OUTPUT_PATH < $OUTPUT_PATH
