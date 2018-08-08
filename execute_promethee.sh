2ec070582987bba58a9c45cc49fe43b8b30a610e
Filipe Mendonça <filipetrm@gmail.com>
2018-08-08 09:09:38 -0300

    removing cds

--- execute_promethee.sh
+++ execute_promethee.sh
@@ -1,16 +1,10 @@
-HOME=$PWD
 PROGRAM_PATH=$1
-EXECUTABLE=$(basename $PROGRAM_PATH)
 INPUT_PATH=$2
 OUTPUT_PATH=$3
 EXPECTED_OUTPUT_PATH=$4
 
-cd $(dirname $PROGRAM_PATH)
-./$EXECUTABLE < $INPUT_PATH > $OUTPUT_PATH
+# executes the promethee with given inputs and output paths
+/$PROGRAM_PATH < $INPUT_PATH > $OUTPUT_PATH
 
-
-cd $HOME/comparator
-echo $PWD
-
-g++ -std=c++14 comparator.cpp -o comparator
-./comparator < $EXPECTED_OUTPUT_PATH < $OUTPUT_PATH
+# executes the comparator with given the expected output path
+./diff/comparator < $EXPECTED_OUTPUT_PATH < $OUTPUT_PATH
